60272a9bfc77f2acac32418edcc586a99df4fea1
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2024-08-04 12:22:52 +0300

    add api for todo

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+STATUS_CHOICES = (
+    ('DONE', 'DONE'),
+    ('INPROGRESS', 'INPROGRESS'),
+)
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    status = models.CharField(max_length=40, choices= STATUS_CHOICES)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.title
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
